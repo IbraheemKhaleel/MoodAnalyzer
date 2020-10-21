e44fa61c9f1634c7dbeeb80af3d0afed80b8fbf3
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-22 00:00:56 +0530

    Added : Welcome message

---
+++ test/java/com/bridgelabz/MoodAnalyzerTest/MoodAnalyzerTest.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.MoodAnalyzerTest;
+
+import org.junit.Test;
+
+public class MoodAnalyzerTest
+{
+	@Test
+	public void Welcome()
+	{
+		System.out.println(" Welcome to mood analyzer ");
+	}
+}
