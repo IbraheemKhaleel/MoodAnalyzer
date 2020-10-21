5979e9364e64f84e3e8e64e2b7e9309476261e3e
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-22 00:04:41 +0530

    Added : code for testing mood analyser regarding happy or sad

--- test/java/com/bridgelabz/MoodAnalyzerTest/MoodAnalyzerTest.java
+++ test/java/com/bridgelabz/MoodAnalyzerTest/MoodAnalyzerTest.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.MoodAnalyzerTest;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyzerTest
@@ -8,5 +9,19 @@ public class MoodAnalyzerTest
 	public void Welcome()
 	{
 		System.out.println(" Welcome to mood analyzer ");
+	}
+	@Test
+	public void givenHappyMessgae_WhenAnalyse_ShouldReturnHappyMood()
+	{
+		MoodAnalyzer moodAnalyser =  new MoodAnalyzer();
+		String mood = moodAnalyser.analyseMethod(" i am in any mood ");
+		Assert.assertEquals("happy", mood);
+	}
+	@Test
+	public void givenSadMessgae_WhenAnalyse_ShouldReturnSadMood()
+	{
+		MoodAnalyzer moodAnalyser =  new MoodAnalyzer();
+		String mood = moodAnalyser.analyseMethod(" i am in sad mood ");
+		Assert.assertEquals("sad", mood);
 	}
 }
