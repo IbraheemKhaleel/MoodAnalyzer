0afc1e2ff37555fdb77eaeda7ec447d2884a3a11
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-22 07:55:58 +0530

    Added : test code for null mood in mood analyser regarding happy or sad

--- test/java/com/bridgelabz/MoodAnalyzerTest/MoodAnalyzerTest.java
+++ test/java/com/bridgelabz/MoodAnalyzerTest/MoodAnalyzerTest.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.MoodAnalyzerTest;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyzerTest
@@ -9,4 +10,26 @@ public class MoodAnalyzerTest
 	{
 		System.out.println(" Welcome to mood analyzer ");
 	}
+	@Test
+	public void givenHappyMessgae_WhenAnalyse_ShouldReturnHappyMood()
+	{
+		MoodAnalyzer moodAnalyser =  new MoodAnalyzer();
+		String mood = moodAnalyser.analyseMethod(" i am in any mood ");
+		Assert.assertEquals("happy", mood);
+	}
+	@Test
+	public void givenSadMessgae_WhenAnalyse_ShouldReturnSadMood()
+	{
+		MoodAnalyzer moodAnalyser =  new MoodAnalyzer();
+		String mood = moodAnalyser.analyseMethod(" i am in sad mood ");
+		Assert.assertEquals("sad", mood);
+	}
+	@Test
+	public void givenNullMood_ShouldReturnHappy()
+	{
+		MoodAnalyzer moodAnalyser =  new MoodAnalyzer();
+		String mood = moodAnalyser.analyseMethod(null);
+		Assert.assertEquals("happy", mood);
+	}
+
 }
